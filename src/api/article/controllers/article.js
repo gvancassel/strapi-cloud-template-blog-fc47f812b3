6038b1ee26dfd301778df8e999396749105931a1
geoffreyvancassel <gvancassel@gmail.com>
2025-08-09 17:21:42 +0200

    update

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -6,4 +6,36 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        cover: true,
+        thumbnail: true,
+        category: true,
+        author: { populate: '*' },
+        blocks: { populate: '*' },
+      },
+    };
+
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        cover: true,
+        thumbnail: true,
+        category: true,
+        author: { populate: '*' },
+        blocks: { populate: '*' },
+      },
+    };
+
+    const { data, meta } = await super.findOne(ctx);
+    return { data, meta };
+  },
+}));
